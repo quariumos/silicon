1aaab2761bba70f50d466973ccf6b1157ac5b11b
qt-sergal, aka Valentine Popov <generic@srgl.cc>
2020-04-02 14:42:16 +0300

    Removed an unneeded dependency

--- src/include/other/async.h/async/examples.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EXAMPLES_H__
-#define __EXAMPLES_H__
-
-extern void example_small(int);
-extern int example_buffer(void);
-extern int example_codelock(void);
-
-#endif
--- src/include/other/async.h/async/main.c
+++
@@ -1,9 +0,0 @@
-
-#include "examples.h"
-
-int main(void) {
-	example_small(200);
-	example_buffer();
-	example_codelock();
-	return 0;
-}
